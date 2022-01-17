0d1078016c4a7a6f5048fca120e071931fb88be2
saeem92 <93969890+saeem92@users.noreply.github.com>
2022-01-18 01:39:38 +0530

    Initial Commit

---
+++ Destini-iOS13/Controller/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Destini-iOS13
+//
+//  Created by saeem  on 18/01/22.
+//  Copyright © 2022 Saeem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var storyLabel: UILabel!
+    @IBOutlet weak var choice1Button: UIButton!
+    @IBOutlet weak var choice2Button: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+
+}
+
---
+++ Destini-iOS13/Model/StoryBrain.swift
@@ -0,0 +1,11 @@
+//
+//  StoryBrain.swift
+//  Destini-iOS13
+//
+//  Created by saeem  on 18/01/22.
+//  Copyright © 2022 Saeem. All rights reserved.
+//
+
+import Foundation
+
+
